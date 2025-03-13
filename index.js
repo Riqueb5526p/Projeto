879343f4af0aa03a500e1162e72757091a52bc10
Henrique Bandeira dos Santos <89607828+Riqueb5526p@users.noreply.github.com>
2025-03-13 11:46:23 -0300

    teste

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const toggleButton = document.getElementById('toggleButton');
-
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById('toggleButton');
   
-  if (document.body.classList.contains('dark-mode')) {
-    toggleButton.textContent = 'Modo Claro';
-  } else {
-    toggleButton.textContent = 'Modo Escuro';
-  }
-});
+    toggleButton.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+      
+      if (document.body.classList.contains('dark-mode')) {
+        toggleButton.textContent = 'Modo Claro';
+      } else {
+        toggleButton.textContent = 'Modo Escuro';
+      }
+    });
+  });
+  
