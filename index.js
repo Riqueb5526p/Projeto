b7f1bb8a90957e48825ce7b0d0ce01f9285484fc
Henrique Bandeira dos Santos <89607828+Riqueb5526p@users.noreply.github.com>
2025-03-13 11:43:20 -0300

    teste modo claro

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  if (document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = 'Modo Claro';
+  } else {
+    toggleButton.textContent = 'Modo Escuro';
+  }
+});
