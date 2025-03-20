8b938491276616167057af71ce5bc5c70001becb
Henrique Bandeira dos Santos <89607828+Riqueb5526p@users.noreply.github.com>
2025-03-20 11:48:01 -0300

    arrumar js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     toggleButton.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
-  
+
         // Alterna o ícone do botão entre Lua e Sol
         const icon = toggleButton.querySelector('i');
         if (body.classList.contains('dark-mode')) {
@@ -16,4 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
             icon.classList.replace('bi-sun', 'bi-moon'); // Sol para Lua
         }
     });
-  });
+
+    function adjustFontSize(scale) {
+        currentFontSize *= scale; // Ajusta o tamanho da fonte
+        document.body.style.fontSize = `${currentFontSize}em`;
+
+        // Ajustar tamanho das imagens proporcionalmente
+        document.querySelectorAll("img").forEach(img => {
+            img.style.width = `${img.naturalWidth * currentFontSize}px`;
+            img.style.height = `${img.naturalHeight * currentFontSize}px`;
+        });
+    }
+
+    increaseFontButton.addEventListener('click', () => adjustFontSize(1.1)); // Aumenta 10%
+    decreaseFontButton.addEventListener('click', () => adjustFontSize(0.9)); // Diminui 10%
+});
